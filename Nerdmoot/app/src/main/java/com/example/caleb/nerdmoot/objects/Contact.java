13fb0d957a0bf97fb23d6aaa03e30d8af3352345
caarce3952 <caarce3952@ung.edu>
2017-04-11 22:03:28 -0400

    Update Contact.java

--- Nerdmoot/app/src/main/java/com/example/caleb/nerdmoot/objects/Contact.java
+++ Nerdmoot/app/src/main/java/com/example/caleb/nerdmoot/objects/Contact.java
@@ -5,4 +5,31 @@ package com.example.caleb.nerdmoot.objects;
  */
 
 public class Contact {
+    private String name;
+    private String number;
+
+    public Contact(){
+
+    }
+
+    public Contact(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getNumber(){
+        return number;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setNumber(String n){
+        number = n;
+    }
 }
